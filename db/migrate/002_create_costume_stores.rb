bccdd84d855264f4c3c5a11f4b0b621e4b801724
Nissan Holzer <nissan.holzer@gmail.com>
2019-04-08 00:37:34 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,2 +1,10 @@
 # Create your costume_stores migration here
 class CostumeStores < ActiveRecord::Migration[5.1]
+  def change 
+    create_table :costume_stores do |t|
+      t.string :name 
+      t.string :location 
+      t.integer :number_of_customes
+    end 
+  end 
+end 
