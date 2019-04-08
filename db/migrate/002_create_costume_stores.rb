a2c7951ae9b498f2a4cc6e0e04579986622cef87
Nissan Holzer <nissan.holzer@gmail.com>
2019-04-08 00:45:58 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -8,7 +8,7 @@ class CostumeStores < ActiveRecord::Migration[5.1]
       t.integer :number_of_employees
       t.boolean :still_in_business
       t.datetime :opening_time
-      t.string :closing_time
+      t.datetime :closing_time
     end 
   end 
 end 
