b69f840b9b2401cd12b432650b866772ba83c78e
Nissan Holzer <nissan.holzer@gmail.com>
2019-04-08 00:43:00 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,2 +1,12 @@
 # Create your haunted_houses migration here
 class HauntedHouse < ActiveRecord::Migration[5.1]
+  def change 
+    create_table :haunted_houses |t|
+      t.string :name 
+      t.string :location
+      t.string :theme
+      t.integer :price 
+      t.
+    end 
+  end 
+end 
